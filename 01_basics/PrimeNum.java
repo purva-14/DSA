6cefb28a629c17ada5f1713f3d4f0e4aec128419
purva-14 <140138406+purva-14@users.noreply.github.com>
2026-02-19 05:53:53 +0000

    prime num

--- 01_basics/PrimeNum.java
+++ 01_basics/PrimeNum.java
@@ -1,6 +1,6 @@
 public class PrimeNum{
     public static void main(String[] args){
-        int num=27;
+        int num=5;
         int cnt=0;
         for(int i=1;i<=num;i++){
             if(num%i==0){
