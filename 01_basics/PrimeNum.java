f546351688decdf4ada3e5649712c5d23c07eb8d
purva-14 <140138406+purva-14@users.noreply.github.com>
2026-02-19 06:00:41 +0000

    prime

--- 01_basics/PrimeNum.java
+++ 01_basics/PrimeNum.java
@@ -1,5 +1,6 @@
 public class PrimeNum{
     public static void main(String[] args){
+        //this is a brute force approach
         int num=5;
         int cnt=0;
         for(int i=1;i<=num;i++){
@@ -9,5 +10,9 @@ public class PrimeNum{
             }
         }
        System.out.println(cnt==2?"PRIME NUMBER":"NOT A PRIME NUMBER");
+
+       //optamized approach
+       //loop should run only till the sqrt of the NUMBER as after that the
+       //factors starts to repeat itself
     } 
 }
