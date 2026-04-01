dd3677f918b04fca40767a074ce7e18ea42b56d9
purva-14 <140138406+purva-14@users.noreply.github.com>
2026-04-01 05:50:54 +0000

    board problem

---
+++ recursion/boardproblem.java
@@ -0,0 +1,15 @@
+public class boardproblem{
+    static void boardProblem(int currentValue,int endValue,String result){
+        if(currentValue==endValue){
+            System.out.println(result+",");
+            return;
+        }
+        
+        for(int dice=1;dice<=6;dice++){
+            boardProblem(currentValue+dice,endValue,result);
+        }
+    }
+    public static void main(String[] args){
+        boardProblem(0,10,"");
+    }
+}
